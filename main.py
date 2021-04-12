a3e25777d5a6fc5a023c9892811360164ffd77ea
Mayank Thakur <mamayankt.2002@gmail.com>
2021-04-12 10:26:18 +0300

    scrapes all data for my senior class + all theri data is included here

--- main.py
+++ main.py
@@ -4,68 +4,85 @@ import time
 import json
 import ast
 
-branch_dict = {'bph':50, 'bms':38, 'bma':50, 'bme':123}
+#branch_dict = {'bph':50, 'bms':38, 'bma':50, 'bme':123}
+branch_dict = (45,55) #45 and 55 are also branches but fuck them lol
+
 driver = webdriver.Firefox()
+#driver.get("http://59.144.74.15/scheme20/studentresult/details.asp") #my year
+driver.get("http://59.144.74.15/scheme19/studentresult/details.asp") #senior year
 
 for branch in branch_dict:
 
     start = time.time()
-    write_file = "D:\Documents\Code\Python\gradesScraper\\" + branch + ".json"
-
+    write_file = "D:\Documents\Code\Python\gradesScraper\\" + str(branch) + ".json"
+    error_arr = [0,0,0,0]
+    # this block is neccesary in case the internet goes out
     try:
         file1 = open(write_file, "r")
         contents1 = file1.read()
         roll_dict = ast.literal_eval(contents1)
         file1.close()
+        print(roll_dict)
     except:
         roll_dict = {}
+        print(roll_dict)
 
-    try:
-        for i in range(1, branch_dict[branch]+1):
+    try: # this try block is needed in case the internet goes out or the computer goes to sleep
+        #the for loop if I wanna iterate over a dictionary
+        '''for i in range(1, branch_dict[branch]+1): #make this a while loop , which is dependent on error_arr
+            if error_arr == [1,1,1,1]:
+                break'''
+        roll = 1
+        while error_arr != [1,1,1,1]:
 
             tables_arr = []
             subject_dict = {}
-            roll_no = "20" + branch + '{0:03}'.format(i)
-
-            driver.get("http://59.144.74.15/scheme20/studentresult/details.asp")
+            roll_no = "19" + str(branch) + '{0:02}'.format(roll)
 
             login = driver.find_element_by_name('RollNumber')
             login.clear()
             login.send_keys(roll_no)
             login.send_keys(Keys.RETURN)
 
-            time.sleep(3)
+            time.sleep(3) #implement explicit waiting
 
             try:
                 driver.find_element_by_tag_name("h2")
                 print(roll_no + " Doesn't exist!")
+                error_arr[roll%4] = 1
 
             except:
+                error_arr[roll%4] = 0
                 tables = driver.find_elements_by_class_name("ewTable")
-
                 for item in tables:
                     thing = item.text.split("\n")
                     tables_arr.append(thing)
 
-                for i in tables_arr[1][1:]:
-                    arr = i.split(" ")
-                    subject_dict[str(arr[-4])] = [int(arr[-3]), int(arr[-1])]
+                try:
+                    j = 1
+                    while True:
+                        for i in tables_arr[j][1:]:
+                            arr = i.split(" ")
+                            subject_dict[str(arr[-4])] = [int(arr[-3]), int(arr[-1])]
+                        j += 2
 
-                roll_dict[roll_no] = subject_dict
-                print(roll_no + " done!")
-                time.sleep(5)
+                except Exception as e:
+                    #print(e)
+                    roll_dict[roll_no] = subject_dict
+                    print(roll_no + " done!")
+                    time.sleep(3)
 
             finally:
                 driver.back()
+                roll += 1
 
         end = time.time()
         print('This operation took: ' + str(end-start))
-
         a_file = open(write_file, "w")
         json.dump(roll_dict, a_file)
         a_file.close()
 
-    except:
-        a_file = open(write_file, "w")
-        json.dump(roll_dict, a_file)
-        a_file.close()
+    except Exception as e:
+        print(e)
+        end = time.time()
+        print('This operation took: ' + str(end-start))
--- visualizer.py
+++ visualizer.py
@@ -1,8 +1,9 @@
 import json
 import ast
 import matplotlib.pyplot as plt
+import numpy as np
 
-file = open("D:\Documents\Code\Python\gradesScraper\\collective.json", "r")
+file = open("D:\Documents\Code\Python\gradesScraper\\20sem1\collective.json", "r")
 contents = file.read()
 main_dict = ast.literal_eval(contents)
 file.close()
@@ -24,22 +25,26 @@ for student in main_dict:
         freq_arr.append(summ/26)
     else:
         freq_arr.append(summ/24)
+    if (summ/24 <= 10) and (summ/24 >= 9.5):
+        print(student)
 
 print(len(freq_arr))
 
 thing = 0
 
 for cg in freq_arr:
-    if cg < 8.25:
+    if cg < 9.2:
         thing += 1
 print(thing/len(freq_arr))
 
 i = 1
-for subject in sub_dict:
+'''for subject in sub_dict:
     plot = plt.figure(i)
-    plt.hist(sub_dict[subject], bins = 30)
+    print(int(np.max(sub_dict[subject])))
+    print(sub_dict[subject])
+    plt.hist(sub_dict[subject], bins = int(np.max(sub_dict[subject])))
     plt.title(subject)
-    i += 1
+    i += 1'''
 
 plot = plt.figure(len(sub_dict)+2)
 plt.hist(freq_arr, bins = 30)
